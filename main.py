85c32955dd9efca3df7d87c644018a263d77b13b
jeffrey-arnold <husain.m.koser@gmail.com>
2026-07-12 12:46:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+
 import streamlit as st
 
-st.set_page_config(initial_sidebar_state="collapsed")
+st.set_page_config(page_title="My App", layout="wide")
 
-# Hide watermark with CSS
-st.markdown("""
-    <style>
-        .stAppViewContainer footer {display: none;}
-    </style>
-""", unsafe_allow_html=True)
+# This hides the footer watermark and the top-right hamburger menu completely
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
 
 col1, col2 = st.columns([1, 4])
 
@@ -15,6 +20,7 @@ with col1:
     st.title('Bite Inc')
 
 with col2:
-    st.image("biteinc.jpg.png", width=100)  # Relative path!
+    st.image("C:\Users\Husain\Desktop\biteinc.jpg.png", width=100)
 
 st.write('Welcome to the Bite Inc official website, where you can explore our menu, order your delicious food, and more!')
+
