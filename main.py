3afc1c9d712cf11d9f4f3e2033f7b549ed679282
jeffrey-arnold <husain.m.koser@gmail.com>
2026-07-12 00:16:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import streamlit as st
 
 col1, col2 = st.columns([1, 4])
 
-st.title('Bite Inc')
-st.image(r"C:\Users\Husain\Desktop\biteinc.jpg.png", width=100)
+with col1:
+    st.title('Bite Inc')
+
+with col2:
+    st.image(r"C:\Users\Husain\Desktop\biteinc.jpg.png", width=100)
 
 st.write('Welcome to the Bite Inc official website, where you can explore our menu, order your delicious food, and more!')
