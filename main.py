b54d6b7aa26aa0f2481a1cca750df0a02adbd4c8
jeffrey-arnold <husain.m.koser@gmail.com>
2026-07-12 00:28:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
+st.set_page_config(initial_sidebar_state="collapsed")
+
+# Hide watermark with CSS
+st.markdown("""
+    <style>
+        .stAppViewContainer footer {display: none;}
+    </style>
+""", unsafe_allow_html=True)
+
 col1, col2 = st.columns([1, 4])
 
 with col1:
